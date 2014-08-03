58c5f31c7cdbcc65de8fb235eb4ec35c913a899c
Brian Oluwo <broluwo@umd.edu>
2014-08-03 19:13:19 -0400

    Adding server/main.go

---
+++ server/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/broluwo/Scoutmaster9000/structs"
+	"github.com/gorilla/mux"
+)
+
+var (
+	routes = []structs.Route{
+		Route{"/user/{name:[a-z]+}", userHandler, []string{"GET", "POST"}},
+		Route{"/team/{teamNum:[0-9]+}", teamHandler, []string{"GET", "POST"}},
+	}
+)
+
+func main() {
+	http.Handle("/", initHandlers())
+	log.Println("Listening...")
+	http.ListenAndServe(":9000", nil)
+}
+func initHandlers() *mux.Router {
+	router := mux.NewRouter()
+	for _, value := range routes {
+		router.HandleFunc(value.Route, value.Handler).Methods(value.Methods...)
+	}
+	return router
+}
+
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func teamHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println("We out here.")
+	params := mux.Vars(req)
+	name := params["teamNum"]
+	w.Write([]byte("Hello " + name))
+}
+func userHandler(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	name := params["name"]
+	w.Write([]byte("Hello " + name))
+}
